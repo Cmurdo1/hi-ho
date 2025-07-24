1023f67f871acdcddd3055a2c23a1b4288fc7543
Cory Murdoch <corymurdoch49@gmail.com>
2025-07-24 07:09:02 +0000

    so is the keys good? switvh the vite to next in the keys remember

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -51,13 +51,16 @@ export default function MainLayout({
             <SidebarMenu>
               {navItems.map((item) => (
                 <SidebarMenuItem key={item.href}>
-                  <Link href={item.href} passHref>
+                  <Link href={item.href} passHref legacyBehavior>
                     <SidebarMenuButton
+                      asChild
                       isActive={pathname === item.href}
                       tooltip={item.label}
                     >
-                      <item.icon />
-                      <span>{item.label}</span>
+                      <a>
+                        <item.icon />
+                        <span>{item.label}</span>
+                      </a>
                     </SidebarMenuButton>
                   </Link>
                 </SidebarMenuItem>
@@ -67,10 +70,12 @@ export default function MainLayout({
           <SidebarFooter>
             <SidebarMenu>
               <SidebarMenuItem>
-                 <Link href="#" passHref>
-                    <SidebarMenuButton tooltip="Settings">
+                 <Link href="#" passHref legacyBehavior>
+                    <SidebarMenuButton asChild tooltip="Settings">
+                      <a>
                         <Settings />
                         <span>Settings</span>
+                      </a>
                     </SidebarMenuButton>
                   </Link>
               </SidebarMenuItem>
