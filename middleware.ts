821943a4f6f32a709a2f6f79f760e1e2c3d93835
Cory Murdoch <corymurdoch49@gmail.com>
2025-07-25 05:09:20 +0000

    what to put as origional origin? https://honestinvoice.com?

--- middleware.ts
+++ middleware.ts
@@ -59,8 +59,11 @@ export async function middleware(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser()
 
-  // if user is not signed in and the current path is not /login, redirect the user to /login
-  if (!user && request.nextUrl.pathname !== '/login') {
+  // Define public routes
+  const publicRoutes = ['/login', '/pricing'];
+
+  // if user is not signed in and the current path is not a public route, redirect the user to /login
+  if (!user && !publicRoutes.includes(request.nextUrl.pathname)) {
     return NextResponse.redirect(new URL('/login', request.url))
   }
 
